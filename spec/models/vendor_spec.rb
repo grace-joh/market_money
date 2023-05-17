d9660e1c0311cd991f68e5539ac4a0048120eb15
Grace Joh <graceehjoh@gmail.com>
2023-05-17 00:25:11 -0500

    Feat/Test: Vendor market_vendors dependent destroy

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Vendor, type: :model do
   describe 'relationships' do
-    it { should have_many(:market_vendors) }
+    it { should have_many(:market_vendors).dependent(:destroy) }
     it { should have_many(:markets).through(:market_vendors) }
   end
 
