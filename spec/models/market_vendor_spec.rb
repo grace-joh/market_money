474f052ef6dd5c205d8ef83bc2eb01a244e4dce8
Grace Joh <graceehjoh@gmail.com>
2023-05-17 15:15:29 -0500

    Feat/Test: MarketVendor custom validation for unique_association

--- spec/models/market_vendor_spec.rb
+++ spec/models/market_vendor_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe MarketVendor, type: :model do
     it { should belong_to(:market) }
     it { should belong_to(:vendor) }
   end
+
+  describe 'validations' do
+    it 'validates unique association' do
+      market1 = create(:market)
+      vendor1 = create(:vendor)
+      m1 = create(:market_vendor, market: market1, vendor: vendor1)
+
+      expect(m1).to be_valid
+      expect { create(:market_vendor, market: market1, vendor: vendor1) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
