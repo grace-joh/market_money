d87574934703d1a2ee01f3f177986ba8aa3f9735
Grace Joh <graceehjoh@gmail.com>
2023-05-18 17:11:02 -0500

    Feat/Test: add scopes to search Markets

--- app/models/market.rb
+++ app/models/market.rb
@@ -2,6 +2,10 @@ class Market < ApplicationRecord
   has_many :market_vendors
   has_many :vendors, through: :market_vendors
 
+  scope :search_by_name, ->(name) { where('name ILIKE ?', "%#{name}%") }
+  scope :search_by_city, ->(city) { where('city ILIKE ?', "%#{city}%") }
+  scope :search_by_state, ->(state) { where('state ILIKE ?', "%#{state}%") }
+
   def vendor_count
     vendors.count
   end
--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -6,6 +6,30 @@ RSpec.describe Market, type: :model do
     it { should have_many(:vendors).through(:market_vendors) }
   end
 
+  describe 'scopes' do
+    before(:each) do
+      market_search_data
+    end
+
+    describe 'search_by_name' do
+      it 'returns markets searched by name mark' do
+        expect(Market.search_by_name('mark')).to eq([@market1, @market2, @market3, @market4, @market5, @market6])
+      end
+    end
+
+    describe 'search_by_city' do
+      it 'returns markets searched by city ville' do
+        expect(Market.search_by_city('ville')).to eq([@market2, @market4, @market5])
+      end
+    end
+
+    describe 'search_by_state' do
+      it 'returns markets searched by state co' do
+        expect(Market.search_by_state('co')).to eq([@market4, @market5, @market6])
+      end
+    end
+  end
+
   describe 'instance methods' do
     describe 'vendor_count' do
       it 'returns the count of vendors for a market' do
