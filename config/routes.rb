25b21a040ad825433a788383218851b01d8c8abe
Grace Joh <graceehjoh@gmail.com>
2023-05-18 20:00:57 -0500

    Add markets search route

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v0 do
-      resources :markets, only: [:index, :show] do
+      resources :markets, only: [:index, :show, :search] do
         resources :vendors, only: [:index], controller: 'markets/vendors'
+        get 'search', on: :collection
       end
       resources :vendors, only: [:show, :create, :update, :destroy]
       resources :market_vendors, only: [:create]
