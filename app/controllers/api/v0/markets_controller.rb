5557793dbed24c2d19dffbcf6a92b9cb9c26f397
Grace Joh <graceehjoh@gmail.com>
2023-05-18 20:07:41 -0500

    Feat/Test: Markets controller search action with params and error handling

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -9,9 +9,26 @@ class Api::V0::MarketsController < ApplicationController
     render json: MarketSerializer.new(@market)
   end
 
+  def search
+    if (params[:state].nil? && params[:city].present?) || search_params.empty?
+      render_invalid_search_response
+    else
+      render json: MarketSerializer.new(Market.search_all(search_params))
+    end
+  end
+
   private
 
   def find_market
     @market = Market.find(params[:id])
+  end
+
+  def search_params
+    params.permit(:state, :city, :name)
+  end
+
+  def render_invalid_search_response
+    message = "Invalid set of parameters. Please provide a valid set of parameters to perform a search with this endpoint."
+    render json: ErrorSerializer.serialize(message), status: 422
   end
 end
