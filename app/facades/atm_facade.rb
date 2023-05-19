6b46da865e2f6ef04f01be74444329433df3a40e
Grace Joh <graceehjoh@gmail.com>
2023-05-18 23:49:19 -0500

    Refactor: facade with Atm PORO

--- app/facades/atm_facade.rb
+++ app/facades/atm_facade.rb
@@ -3,13 +3,7 @@ class AtmFacade
     json = service.nearby_atms(lat, lon)[:results]
 
     json.map do |atm_data|
-      {
-        name: atm_data[:poi][:name],
-        address: atm_data[:address][:freeformAddress],
-        lat: atm_data[:position][:lat],
-        lon: atm_data[:position][:lon],
-        distance: (atm_data[:dist] / 1609.344)
-      }
+      Atm.new(atm_data)
     end
   end
 
--- spec/facades/atm_facade_spec.rb
+++ spec/facades/atm_facade_spec.rb
@@ -11,20 +11,20 @@ RSpec.describe AtmService do
           expect(atm_data.count).to eq(10)
 
           first_atm = atm_data.first
-          expect(first_atm).to be_a(Hash)
-          expect(first_atm[:name]).to eq('ATM')
-          expect(first_atm[:address]).to eq('3902 Central Avenue Southeast, Albuquerque, NM 87108')
-          expect(first_atm[:lat]).to eq(35.079044)
-          expect(first_atm[:lon]).to eq(-106.60068)
-          expect(first_atm[:distance]).to eq(0.10548811068360774)
+          expect(first_atm).to be_an(Atm)
+          expect(first_atm.name).to eq('ATM')
+          expect(first_atm.address).to eq('3902 Central Avenue Southeast, Albuquerque, NM 87108')
+          expect(first_atm.lat).to eq(35.079044)
+          expect(first_atm.lon).to eq(-106.60068)
+          expect(first_atm.distance).to eq(0.10548811068360774)
 
           last_atm = atm_data.last
-          expect(last_atm).to be_a(Hash)
-          expect(last_atm[:name]).to eq('ATM at Efx')
-          expect(last_atm[:address]).to eq('4720 Central Avenue Southeast, Albuquerque, NM 87108')
-          expect(last_atm[:lat]).to eq(35.078207)
-          expect(last_atm[:lon]).to eq(-106.591563)
-          expect(last_atm[:distance]).to eq(0.504645272856518)
+          expect(last_atm).to be_an(Atm)
+          expect(last_atm.name).to eq('ATM at Efx')
+          expect(last_atm.address).to eq('4720 Central Avenue Southeast, Albuquerque, NM 87108')
+          expect(last_atm.lat).to eq(35.078207)
+          expect(last_atm.lon).to eq(-106.591563)
+          expect(last_atm.distance).to eq(0.504645272856518)
         end
       end
     end
